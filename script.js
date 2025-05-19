dbdf9b2d3461fa930d152b3ab440026635b9a4cf
AhtnamasAyirpuna <samanthaanupriya98@gmail.com>
2025-05-19 23:04:28 +0800

    Modified script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello!');
+const amountInput = document.getElementById('amount-to-convert');
+const fromCurrency = document.getElementById('from-currency');
+const toCurrency = document.getElementById('to-currency');
+const convert = document.getElementById('convert');
+const reset = document.getElementById('reset');
+const result = document.getElementById('currency-result');
